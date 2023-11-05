a9bebd78ff8fe6120e76f2ca09de02dc4dd44af7
Tim Wee <tim.shawn@gmail.com>
2023-11-04 21:11:58 -0400

    <your_commit_message>

---
+++ cmd/mybittorrent/trackerclient.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/dghubble/sling"
+	"github.com/jackpal/bencode-go"
+)
+
+func GetPeers(torrentFile TorrentFile, infoHash []byte) (TrackerResponse, error) {
+	params := DefaultTrackerClientParams(string(infoHash), torrentFile.Info.Length)
+	req, err := sling.New().Get(torrentFile.Announce).QueryStruct(params).Request()
+	if err != nil {
+		return TrackerResponse{}, err
+	}
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Printf("client: error making http request: %s\n", err)
+		return TrackerResponse{}, err
+	}
+	defer res.Body.Close()
+	resBody, err := io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Printf("client: could not read response body: %s\n", err)
+		return TrackerResponse{}, err
+	}
+	// fmt.Printf("client: response body: %s\n", resBody)
+
+	var trackerResp TrackerResponse
+	if err := bencode.Unmarshal(bytes.NewReader(resBody), &trackerResp); err != nil {
+		return TrackerResponse{}, err
+	}
+	return trackerResp, nil
+}
---
+++ cmd/mybittorrent/types.go
@@ -0,0 +1,42 @@
+package main
+
+import "strconv"
+
+type TorrentFileInfo struct {
+	Length      int    `bencode:"length"`
+	Name        string `bencode:"name"`
+	PieceLength int    `bencode:"piece length"`
+	Pieces      string `bencode:"pieces"`
+}
+
+type TorrentFile struct {
+	Announce string
+	Info     TorrentFileInfo
+}
+
+type TrackerResponse struct {
+	Interval int    `bencode:"interval"`
+	Peers    string `bencode:"peers"`
+}
+
+type TrackerClientParams struct {
+	InfoHash   string `url:"info_hash,omitempty"`
+	PeerId     string `url:"peer_id,omitempty"`
+	Port       string `url:"port,omitempty"`
+	Uploaded   string `url:"uploaded,omitempty"`
+	Downloaded string `url:"downloaded,omitempty"`
+	Left       string `url:"left,omitempty"`
+	Compact    string `url:"compact,omitempty"`
+}
+
+func DefaultTrackerClientParams(infoHash string, fileLength int) *TrackerClientParams {
+	return &TrackerClientParams{
+		InfoHash:   infoHash,
+		PeerId:     "00112233445566778899",
+		Port:       "6881",
+		Uploaded:   "0",
+		Downloaded: "0",
+		Left:       strconv.Itoa(fileLength),
+		Compact:    "1",
+	}
+}
